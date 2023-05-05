caf0dfb56ae136435b71d378aedd42507a92ecd6
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-05-05 07:47:58 +0300

    web_dynamic

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,43 +1,45 @@
 #!/usr/bin/python3
-"""Starts a Flask Web Application"""
-import uuid
-from os import environ
-from flask import Flask, render_template
+""" Starts a Flash Web Application """
 from models import storage
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place
-
+from os import environ
+from flask import Flask, render_template
 app = Flask(__name__)
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
+
 
 @app.teardown_appcontext
 def close_db(error):
-    """Closes the current SQLAlchemy session"""
+    """ Remove the current SQLAlchemy Session """
     storage.close()
 
-@app.route('/0-hbnb/', strict_slashes=False)
+
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Displays a page with information about states, cities, amenities, and places"""
-    all_states = storage.all(State).values()
-    sorted_states = sorted(all_states, key=lambda state: state.name)
-    state_city_list = []
+    """ HBNB is alive! """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    st_ct = []
 
-    for state in sorted_states:
-        state_city_list.append([state, sorted(state.cities, key=lambda city: city.name)])
+    for state in states:
+        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
 
-    all_amenities = storage.all(Amenity).values()
-    sorted_amenities = sorted(all_amenities, key=lambda amenity: amenity.name)
+    amenities = storage.all(Amenity).values()
+    amenities = sorted(amenities, key=lambda k: k.name)
 
-    all_places = storage.all(Place).values()
-    sorted_places = sorted(all_places, key=lambda place: place.name)
+    places = storage.all(Place).values()
+    places = sorted(places, key=lambda k: k.name)
 
-    return render_template('0-hbnb.html',
-                           states=state_city_list,
-                           amenities=sorted_amenities,
-                           places=sorted_places,
-                           cache_id=uuid.uuid4())
+    return render_template('100-hbnb.html',
+                           states=st_ct,
+                           amenities=amenities,
+                           places=places)
+
 
 if __name__ == "__main__":
-    """Main function"""
+    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
